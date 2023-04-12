dce7d1c65a0f00336123ac93018feca6303be8f9
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-04-12 16:01:26 +0700

    create project with nextjs

--- .history/src/pages/_app_20230411234110.tsx
+++ .history/src/pages/_app_20230411234110.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.scss";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 // Create a client
 const queryClient = new QueryClient();
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+
+  <Component {...pageProps} />);
     </QueryClientProvider>
-  );
 }
