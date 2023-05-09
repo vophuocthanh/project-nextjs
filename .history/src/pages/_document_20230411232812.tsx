47840baa31e34961f3b4eb4ad1e28a5be7565061
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-05-09 23:54:53 +0700

    update title

--- .history/src/pages/_document_20230411232812.tsx
+++ .history/src/pages/_document_20230411232812.tsx
@@ -1,14 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>Yariga App</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
